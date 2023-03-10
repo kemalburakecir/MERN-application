222ad5f737615f8fc526ea1e514fab8926cf1d39
Kemal Burak Ecir <kemalburak.ecir@gmail.com>
2023-03-10 07:43:46 -0500

    update

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import React from 'react';
 const App = () => {
     return (
         <div>
-            <h1>App</h1>
+            <h1>App hhahahah</h1>
+            <p>kemal </p>
         </div>
     )
 }
