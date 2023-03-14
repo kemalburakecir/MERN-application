8eeaf073d6b1b3214c1716cba8261a9d716dd5ae
Kemal Burak Ecir <kemalburak.ecir@gmail.com>
2023-03-13 22:33:13 -0400

    components, controller folder, model folder, material ui

--- src/App.js
+++ src/App.js
@@ -1,13 +1,42 @@
-import React from 'react';
+import React from "react";
+import { Container, AppBar, Typography, Grow, Grid } from "@mui/material";
+import memories from "./images/memories.png";
+import Posts from './components/Posts/posts';
+import Form from './components/Form/form';
+import useStyles from './styles'
+
+//Material UI is an open-source React component library that implements Google's Material Design.
+//It includes a comprehensive collection of prebuilt components that are ready for use in production right out of the box.
 
 const App = () => {
-    return (
-        <div>
-            <h1>App hhahahah</h1>
-            <p>kemal </p>
-        </div>
-    )
-}
+    const classes = useStyles();
+  return (
+    <Container maxWidth="lg">
+      <AppBar className={classes.AppBar} position="static" color="inherit">
+        <Typography className={classes.heading} variant="h2" align="center">
+          Kemal Burak Ecir
+        </Typography>
+        <img className={classes.image} src={memories} alt="memories" height="60" />
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid
+            container
+            justify="space-between"
+            alignItems="stretch"
+            spacing={3}
+          >
+            <Grid item xs={12} sm={7}>
+                <Posts />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+                <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
+  );
+};
 
 export default App;
-
---
+++ src/styles.js
@@ -0,0 +1,19 @@
+import { makeStyles } from "@mui/material";
+
+export default makeStyles(() => ({
+  // callback function which immediately returns an object
+  appBar: {
+    borderRadius: 15,
+    margin: "30px 0",
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  heading: {
+    color: "rgba(0,183,255, 1)",
+  },
+  image: {
+    marginLeft: "15px",
+  },
+}));
